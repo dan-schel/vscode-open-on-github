9706905caaf3cdb31e8947719b2555ddcf55c412
dan-schel <me@danschellekens.com>
2025-05-17 16:05:12 +1000

    tiny fix

--- src/extension.ts
+++ src/extension.ts
@@ -12,5 +12,5 @@ export function activate(ctx: vscode.ExtensionContext) {
   buildCommand(ctx, editorPermalink, "copy", "editor", "current-commit");
   buildCommand(ctx, editorDefault, "open", "editor", "current-branch");
   buildCommand(ctx, editorMaster, "open", "editor", "default-branch");
-  buildCommand(ctx, explorerPermalink, "copy", "editor", "current-commit");
+  buildCommand(ctx, explorerPermalink, "copy", "explorer", "current-commit");
 }
