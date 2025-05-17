236bbe0fc6bbef2e6c9be430fbc58dc82eead3c6
dan-schel <me@danschellekens.com>
2025-05-17 13:42:04 +1000

    include line numbers

--- src/utils/build-web-url.ts
+++ src/utils/build-web-url.ts
@@ -15,12 +15,13 @@ export function buildWebUrl(
   if ("error" in repoNameQuery) return { error: repoNameQuery.error };
   const { repoName } = repoNameQuery;
 
-  const path = uri.fsPath;
-  const repoPath = repoUri.fsPath;
+  const path = normalize(uri.fsPath);
+  const repoPath = normalize(repoUri.fsPath);
   if (!path.startsWith(repoPath)) return { error: "uri-outside-repo" };
 
   const relativePath = path.slice(repoPath.length + 1);
-  const webUrl = `https://github.com/${repoName}/blob/${branch}/${relativePath}`;
+  const lineNumbers = createLineNumbersSuffix(lines);
+  const webUrl = `https://github.com/${repoName}/blob/${branch}/${relativePath}${lineNumbers}`;
   return { webUrl };
 }
 
@@ -28,14 +29,13 @@ function extractRepoName(
   cloneUrl: string
 ): { repoName: string } | { error: ErrorType } {
   // HTTP: https://github.com/username/repo.git
-  // SSH:  git@github.com:username/repo.git
-
   const http = /^https?:\/\/([^/]+)\/([^/]+)\/([^/]+)\.git$/gi.exec(cloneUrl);
   if (http != null) {
     if (!http[1].endsWith("github.com")) return { error: "not-github" };
     return { repoName: `${http[2]}/${http[3]}` };
   }
 
+  // SSH: git@github.com:username/repo.git
   const ssh = /^([^@]+)@([^:]+):([^/]+)\/([^/]+)\.git$/gi.exec(cloneUrl);
   if (ssh != null) {
     if (!ssh[2].endsWith("github.com")) return { error: "not-github" };
@@ -43,4 +43,16 @@ function extractRepoName(
   }
 
   return { error: "unknown-scheme" };
+}
+
+function createLineNumbersSuffix(
+  lines: { start: number; end: number } | undefined
+): string {
+  if (lines == null) return "";
+  if (lines.start === lines.end) return `#L${lines.start}`;
+  return `#L${lines.start}-L${lines.end}`;
+}
+
+function normalize(path: string): string {
+  return path.replace(/\/$/g, "");
 }
